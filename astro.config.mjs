a86825bcac604f1cac26de23e9210191a62e8e96
Ioannis Kyriazis <ikyriazis@wpi.edu>
2025-01-27 23:36:31 -0500

    use node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        port: 7006,
-        host: true
-    },
-    preview: {
-        port: 7006,
-        host: true
-    }
+  server: {
+      port: 7006,
+      host: true
+  },
+
+  preview: {
+      port: 7006,
+      host: true
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
